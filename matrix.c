c87b4a257ac8179d7c74400f3ab728227b60b4f1
felipemattosv <felipe.vanetti@gmail.com>
2024-08-04 17:05:55 -0300

    fix: matrix_init

--- matrix.c
+++ matrix.c
@@ -1,15 +1,12 @@
 #include "matrix.h"
 
-Weight** matrix_init(int l, int c) {
-    Weight **m = (Weight **)malloc(l * sizeof(Weight*));
-    for (int i = 0; i < l; i++)
-        m[i] = (Weight *)calloc(c, sizeof(Weight*));
+Weight **matrix_init(int l, int c) {
+    Weight **m = (Weight **)malloc(l * sizeof(Weight *));
+    for (int i = 0; i < l; i++) m[i] = (Weight *)calloc(c, sizeof(Weight));
     return m;
 }
 
-
 void matrix_destroy(Weight **m, int l) {
-    for (int i = 0; i < l; i++)
-        free(m[i]);
+    for (int i = 0; i < l; i++) free(m[i]);
     free(m);
 }
